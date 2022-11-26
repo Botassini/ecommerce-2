3c97d67fc5d450529d4a5b565def8e6b54264f98
Botassini <80798585+Botassini@users.noreply.github.com>
2022-11-26 17:35:01 -0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header >
+        <a href="">FGL Distribuidoracd</a>
+      </header>
+      <main>
+        Lista de Produtos
+      </main>
+    </div>
+  );
+}
+
+export default App;
